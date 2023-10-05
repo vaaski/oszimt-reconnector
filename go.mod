5eec3931f61a662e9000ed8f2ff5087bfe0bbd87
vaaski <admin@vaa.ski>
2023-10-05 13:40:15 +0200

    read, write, encode & decode credentials

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module vaaski/oszimt-reconnector
 
 go 1.21.1
 
-require github.com/PuerkitoBio/goquery v1.8.1
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	golang.org/x/term v0.12.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- oszr.go
+++ oszr.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"bufio"
+	b64 "encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"syscall"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/term"
 )
+
+const CREDENTAIL_FILE = "credentials"
+
+func maybePanic(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func getDoc(url string) *goquery.Document {
 	res, err := http.Get("http://wlan-login.oszimt.de/logon/cgi/index.cgi")
-	if err != nil {
-		log.Fatal(err)
-	}
+	maybePanic(err)
+
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
@@ -20,9 +33,7 @@ func getDoc(url string) *goquery.Document {
 
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	maybePanic(err)
 
 	return doc
 }
@@ -34,10 +45,56 @@ func isLoggedIn() bool {
 	return logoffButton.Length() > 0
 }
 
-func main() {
-	if isLoggedIn() {
-		fmt.Println("logged in")
-	} else {
-		fmt.Println("not logged in")
+func readCredentials() (string, string, error) {
+	data, err := os.ReadFile(CREDENTAIL_FILE)
+	if err != nil {
+		return "", "", err
 	}
+
+	bDecoded, decodeErr := b64.StdEncoding.DecodeString(string(data))
+	if decodeErr != nil {
+		return "", "", decodeErr
+	}
+
+	decoded := string(bDecoded)
+	credentials := strings.Split(string(decoded), ":")
+	return credentials[0], credentials[1], nil
+}
+
+func askForCredentials() {
+	fmt.Print("username: ")
+	reader := bufio.NewReader(os.Stdin)
+	bUsername, err := reader.ReadString('\n')
+	maybePanic(err)
+	username := strings.TrimSpace(string(bUsername))
+
+	fmt.Print("password: ")
+	bPassword, err := term.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	maybePanic(err)
+	password := strings.TrimSpace(string(bPassword))
+
+	joined := strings.Join([]string{username, password}, ":")
+	encoded := b64.StdEncoding.EncodeToString([]byte(joined))
+	writeErr := os.WriteFile(CREDENTAIL_FILE, []byte(encoded), 0644)
+	maybePanic(writeErr)
+}
+
+func main() {
+	// if isLoggedIn() {
+	// 	log.Println("logged in")
+	// } else {
+	// 	log.Println("not logged in")
+	// }
+
+	// askForCredentials()
+
+	username, password, err := readCredentials()
+	if err != nil {
+		askForCredentials()
+		username, password, err = readCredentials()
+		maybePanic(err)
+	}
+
+	log.Println(username, password)
 }
